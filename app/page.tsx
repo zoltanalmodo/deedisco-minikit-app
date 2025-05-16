d456a170f3f2789a314497b83f2c442a023c5f27
zoltan almodo <zoltanalmodo@gmail.com>
2025-05-16 02:54:21 +0200

    Fix TypeScript error related to removeBorder prop

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,6 @@ export default function Home() {
                 images={carousel.images}
                 selectedIndex={selectedImages[index]}
                 onSelect={(imageIndex) => handleImageSelect(index, imageIndex)}
-                removeBorder={true}
               />
             </div>
           ))}
