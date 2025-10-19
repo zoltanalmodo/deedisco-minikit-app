34004c8f866bc5426c45dbd65f8a6ac48c4fa097
zoltan almodo <zoltanalmodo@gmail.com>
2025-10-19 15:27:17 +0200

    Add Fraunces Black font (weight 900) to all H1 elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap" rel="stylesheet" />
         <title>{title}</title>
         <meta name="description" content={description} />
 
